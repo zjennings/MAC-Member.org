53919b1f19c7fed5ad9b6d22d6356997b1e21206
Kyle Hicks <khicks1124@gmail.com>
2016-08-15 22:09:58 -0400

    added initial design for home page

--- MAC/App_Start/BundleConfig.cs
+++ MAC/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace MAC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
