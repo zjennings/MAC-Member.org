3e58ac8eeefce7732dcd60b08fa3d7f142342135
Kyle Hicks <khicks1124@gmail.com>
2016-08-17 21:23:32 -0400

    working

--- MAC/App_Start/BundleConfig.cs
+++ MAC/App_Start/BundleConfig.cs
@@ -24,7 +24,11 @@ namespace MAC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));            
+
+            bundles.Add(new ScriptBundle("~/bundles/morphist").Include(
+                      "~/Scripts/morphist.js"));        
         }
     }
 }
