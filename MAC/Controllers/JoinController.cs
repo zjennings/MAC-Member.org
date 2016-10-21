05d974eea18104721a97e5df8426feef3a40aa5f
Kyle Hicks <Kyle Hicks>
2016-10-20 20:35:01 -0400

    frontend updates

---
+++ MAC/Controllers/JoinController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MAC.Controllers
+{
+    public class JoinController : Controller
+    {
+        // GET: Join
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
