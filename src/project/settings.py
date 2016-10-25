d60f0cc56ebdc7fd459e2516e5a541194fd14385
MattTannahill <matthew.r.tannahill@live.com>
2016-10-24 22:05:21 -0400

    Added MySQL database

--- src/project/settings.py
+++ src/project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '3306'
     }
 }
 
